27e379e9c6753152bd04e9f7478d419c7a7dcaf9
root <root@localhost.localdomain>
2018-10-11 21:38:54 +0900

    practice socket programming

---
+++ hjy/socket/00_select/server.c
@@ -0,0 +1,160 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/time.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#define PORT 4321
+#define MAX 100
+#define BUF_SIZE 1024
+
+int init_network();
+
+int main()
+{
+	
+	int server_sd, max_sd;
+	int client_sd, client_len;
+	struct sockaddr_in client_addr;
+
+	fd_set recv_fds, send_fds, temp_fds;
+	int i, rc;
+	struct timeval timeout;
+
+	int str_len;
+	char msg[BUF_SIZE];
+
+	// init_network( socket --> bind --> listen )
+	if( -1 == (server_sd = init_network()))
+	{
+		printf("error : init_network()\n");
+		return -1;
+	}
+
+	FD_ZERO(&recv_fds);
+	FD_ZERO(&send_fds);
+	
+	client_len = sizeof(client_addr);
+
+	// accept
+	printf("sever_sd : %d\n", server_sd);
+	FD_SET(server_sd, &recv_fds);
+	max_sd = server_sd;	
+
+
+	while(1)
+	{
+		temp_fds = recv_fds;	
+
+		timeout.tv_sec = 5;
+		timeout.tv_usec = 0;
+		
+		printf("select wait..\n");
+		rc = select(max_sd+1, &temp_fds, 0, 0, &timeout);
+		
+		if(rc == -1)
+		{
+			perror("select");
+		}else if(rc == 0)
+		{
+			printf("select : timeout\n");
+		}else 
+		{
+			if(FD_ISSET(server_sd, &temp_fds))
+			{
+				// accept
+				client_sd = accept(server_sd, (struct sockaddr *)&client_addr, &client_len);
+				FD_SET(client_sd, &recv_fds);
+				if(max_sd < client_sd)
+					max_sd = client_sd;
+				printf("connection(%d)\n", client_sd);
+			}
+		}	
+
+		for(i=server_sd+1; i<=max_sd; i++)
+		{
+			if(FD_ISSET(i, &temp_fds))
+			{
+				rc = recv(i, msg, BUF_SIZE, 0);
+				if(rc == 0)
+				{
+					FD_CLR(i, &recv_fds);
+					close(i);
+					if(max_sd <= i)
+						max_sd = i-1;
+					printf("disconnection(%d)\n", i);
+				}else
+				{
+					printf("(%d)recv : %s\n", i, msg);
+					// echo
+					rc = send(i, msg, strlen(msg), 0);
+					if(rc < 0)
+						printf("send : error\n");
+				}
+				memset(msg, '\0', BUF_SIZE);
+			}
+		}
+
+
+	}
+	
+	return 0;
+}
+
+int init_network()
+{
+	/*
+	 * 1.create socket
+	 * 2.set socket option
+	 * 3.bind
+	 * 4.listen
+	 * 5.ready accept
+	 */ 
+ 	int server_sd;
+	int optval = 1;
+	struct sockaddr_in server_addr;
+	
+	// 1.
+	if(-1 == (server_sd = socket(AF_INET, SOCK_STREAM, 0)))
+	{
+		perror("create socket");
+		return -1;
+	}
+
+	// 2.
+	// SO_REUSEDDR : 이미 사용된 주소를 재사용
+	// ex) bind error : Address already in use 
+	if(setsockopt(server_sd, SOL_SOCKET, SO_REUSEADDR, (const char*)&optval, sizeof(optval)) < 0)
+	{
+		perror("setsocket");
+		return -1;
+	}
+	
+	server_addr.sin_family = AF_INET;
+	//server_addr.sin_addr.s_addr = INADDR_ANY;
+	server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	server_addr.sin_port = htons(PORT); // #define PORT 1234
+	
+	// 3.
+	if(-1 == bind(server_sd, (struct sockaddr *)&server_addr, sizeof(server_addr)))
+	{
+		perror("bind");
+		return -1;
+	}
+
+	// 4.
+	// backlog : 5
+	if(-1 == listen(server_sd, 5))
+	{
+		perror("listen");
+		return -1;
+	}
+	
+	printf("-----Listenr on port : %d\n", PORT);
+	printf("-----Waiting for connection\n");
+
+	return server_sd;
+}
